0ac4306c514436d18f653d8e593b9f0e6335d8dc
Сергей <153823538+Sergei-Shmakov@users.noreply.github.com>
2025-08-03 04:38:29 +0700

    accordion

--- js/main.js
+++ js/main.js
@@ -115,4 +115,36 @@
 			updateSubmitButton(form)
 		})
 	})
+	//accordion
+
+	const accordionLists = document.querySelectorAll('.accordion-list')
+
+	accordionLists.forEach(el => {
+		el.addEventListener('click', e => {
+			const accordionList = e.currentTarget
+			const accordionOpenedItem = accordionList.querySelector(
+				'.accordion-list__item--opened'
+			)
+			const accordionOpenedContent = accordionList.querySelector(
+				'.accordion-list__item--opened .accordion__item-text'
+			)
+
+			const accordionControl = e.target.closest('.accordion-list__control')
+			if (!accordionControl) return
+			const accordionItem = accordionControl.parentElement
+			const accordionContent = accordionControl.nextElementSibling
+
+			if (accordionOpenedItem && accordionOpenedItem != accordionItem) {
+				accordionOpenedItem.classList.remove('accordion-list__item--opened')
+				accordionOpenedContent.style.maxHeight = null
+			}
+			accordionItem.classList.toggle('accordion-list__item--opened')
+
+			if (accordionItem.classList.contains('accordion-list__item--opened')) {
+				accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+			} else {
+				accordionContent.style.maxHeight = null
+			}
+		})
+	})
 })()
